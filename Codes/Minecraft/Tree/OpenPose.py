14af4f82f93ef681f27925e566c671635a0afcb4
yamajunn <amiton1115@gmail.com>
2024-07-27 08:20:26 +0000

    2024年7月27日 17:20

--- Codes/Minecraft/Tree/OpenPose.py
+++ Codes/Minecraft/Tree/OpenPose.py
@@ -1,37 +1,26 @@
+import sys
 import cv2
-import mediapipe as mp
+from openpose import pyopenpose as op
 
-# MediaPipeのポーズ推定の初期化
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose()
-mp_draw = mp.solutions.drawing_utils
+# OpenPoseのパラメータ設定
+params = dict()
+params["model_folder"] = "models/"
+
+# OpenPoseの初期化
+opWrapper = op.WrapperPython()
+opWrapper.configure(params)
+opWrapper.start()
 
 # 画像の読み込み
-image_path = './Codes/Minecraft/Tree/image.jpg'
-image = cv2.imread(image_path)
+image_path = "image.jpg"
+imageToProcess = cv2.imread(image_path)
 
-# 画像が正常に読み込まれたか確認する
-if image is None:
-    print(f"指定されたファイル {image_path} が見つかりません。ファイルパスを確認してください。")
-    exit()
+# OpenPoseによるポーズ推定
+datum = op.Datum()
+datum.cvInputData = imageToProcess
+opWrapper.emplaceAndPop([datum])
 
-# 色空間の変換
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-# ポーズ推定の実行
-results = pose.process(image_rgb)
-
-# 推定結果の描画
-if results.pose_landmarks:
-    mp_draw.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-
-# 画像を表示
-cv2.imshow('Pose Detection', image)
-
-# ウィンドウが表示されるまで待機する
-key = cv2.waitKey(0)
-if key == -1:
-    print("キー入力がありません。ウィンドウが表示されていない可能性があります。")
-
-# ウィンドウを閉じる
+# 結果の表示
+cv2.imshow("OpenPose - Tutorial Python API", datum.cvOutputData)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
